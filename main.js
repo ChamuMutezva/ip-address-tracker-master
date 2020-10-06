acd1927b18f970c8d997eb71ebbba3b0b778fdd2
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-10-06 04:25:37 +0200

    feat: display infor template

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
-async function getAddress() {
-    const accessToken = `pk.eyJ1IjoiMTAwMTIxODgwMiIsImEiOiJjam1lZHppNDIwY3JuM3BwZjM3ZnNmZDJ2In0.5BIcQy3_ACs0afqvHmPbVg`
+const form = document.querySelector("form")
+var mymap
+async function getAddress(ipLocator) {
+    const accessToken = `pk.eyJ1IjoiMTAwMTIxODgwMiIsImEiOiJjam1lZHppNDIwY3JuM3BwZjM3ZnNmZDJ2In0.5BIcQy3_ACs0afqvHmPbVg&ipAddress=${ipLocator}`
     const apiKey = "at_XvZLm5dvyEPgWUu3SSWx0TRMNraLr"
-    const ip = await fetch("https://geo.ipify.org/api/v1?apiKey=at_XvZLm5dvyEPgWUu3SSWx0TRMNraLr")
-    const res =await ip.json()
+    const ip = await fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}`)
+    const res = await ip.json()
     console.log(res)
     console.log(res.location)
-    const {lat, lng} = res.location;
+    const { lat, lng } = res.location;
     console.log(lat)
-    var mymap = L.map('mapid').setView([lat, lng], 13);
-
+     mymap = L.map('mapid').setView([lat, lng], 13);
+     L.marker([lat, lng]).addTo(mymap);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: accessToken
-}).addTo(mymap);
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: accessToken
 
+    }).addTo(mymap);
+    //marker.setLatLng([lat, long]) 
+  
+    //
+     
 }
 getAddress()
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputData = document.querySelector("input").value;
+    console.log(inputData)
+   // const inputItem = "8.8.8.8";
+    getAddress2(inputData)
+})
+async function getAddress2(iptracker) {  
+   // const accessToken = `pk.eyJ1IjoiMTAwMTIxODgwMiIsImEiOiJjam1lZHppNDIwY3JuM3BwZjM3ZnNmZDJ2In0.5BIcQy3_ACs0afqvHmPbVg&ipAddress=${iptracker}`
+    const apiKey = "at_XvZLm5dvyEPgWUu3SSWx0TRMNraLr"
+    const ip = await fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${iptracker}`)
+   // https://geo.ipify.org/api/v1?apiKey=at_XvZLm5dvyEPgWUu3SSWx0TRMNraLr&ipAddress=8.8.8.8
+    const res = await ip.json()
+    console.log(res)
+    console.log(res.location)
+    const { lat, lng } = res.location;
+    console.log(lat)
+    console.log(iptracker)
+  // marker.setLatLng([lat, lng]).addTo(mymap)
+   const marker =  L.marker([lat, lng]).addTo(mymap);
+ // marker.flyTo([lat,lng]).addTo(mymap)
+}
+
